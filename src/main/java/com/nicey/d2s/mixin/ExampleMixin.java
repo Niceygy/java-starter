1a080291c1d9ebf72d55629add6f6d7ae2ac4c88
Niceygy <nceygy01@gmail.com>
2022-09-24 18:17:12 +0100

    it works!

--- src/main/java/com/nicey/d2s/mixin/ExampleMixin.java
+++ src/main/java/com/nicey/d2s/mixin/ExampleMixin.java
@@ -6,11 +6,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import static net.minecraft.server.command.CommandManager.*;
+
+
+
 
 @Mixin(TitleScreen.class)
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		Discord2Server.LOGGER.info("This line is printed by an example mod mixin!");
+		Discord2Server.LOGGER.info("Mod loading... Please stand by.");
 	}
 }
